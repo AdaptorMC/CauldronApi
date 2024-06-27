138c7b07daf805ee97aba68f40e6e3c849b10e69
Yunitrish <yunthomas006@gmail.com>
2024-06-27 17:37:06 +0800

    i forgot to update MODID in code script

--- src/main/java/net/adaptor/cauldron/Main.java
+++ src/main/java/net/adaptor/cauldron/Main.java
@@ -10,7 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Main implements ModInitializer {
-	public static final String MOD_ID = "adaptor";
+	public static final String MOD_ID = "cauldron_api";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	public static Identifier id(String name) {
 		return Identifier.of(Main.MOD_ID, name);
