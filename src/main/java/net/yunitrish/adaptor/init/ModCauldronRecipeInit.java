988d632add835768a1ea04075831227503476e43
chai <chainot@hotmail.co.th>
2024-06-27 14:06:50 +0700

    Test stuff

--- src/main/java/net/yunitrish/adaptor/init/ModCauldronRecipeInit.java
+++ src/main/java/net/yunitrish/adaptor/init/ModCauldronRecipeInit.java
@@ -9,14 +9,17 @@ import net.yunitrish.adaptor.recipe.CauldronRecipe;
 public class ModCauldronRecipeInit implements CauldronRecipeProvider {
     @Override
     public void addCauldronRecipes() {
-        CauldronRecipe gravel = new CauldronRecipe(CauldronRecipe.DeviceType.NORMAL, "gravel")
+        CauldronRecipe gravel = new CauldronRecipe(CauldronRecipeRegistry.DeviceType.NORMAL, "gravel")
                 .setRecipeItem(Items.GRAVEL.getDefaultStack())
                 .setResultItem(Items.SAND.getDefaultStack(), Items.FLINT.getDefaultStack());
-        CauldronRecipe sand = new CauldronRecipe(CauldronRecipe.DeviceType.NORMAL,"sand")
+        CauldronRecipe sand = new CauldronRecipe(CauldronRecipeRegistry.DeviceType.NORMAL,"sand")
                 .setRecipeItem(Items.SAND.getDefaultStack())
                 .setResultItem(Items.CLAY.getDefaultStack());
-
+        CauldronRecipe chicken = new CauldronRecipe(CauldronRecipeRegistry.DeviceType.BOILED, "cookChick")
+                .setRecipeItem(Items.CHICKEN.getDefaultStack())
+                        .setResultItem(Items.COOKED_CHICKEN.getDefaultStack());
         CauldronRecipeRegistry.registerRecipe(gravel);
         CauldronRecipeRegistry.registerRecipe(sand);
+        CauldronRecipeRegistry.registerRecipe(chicken);
     }
 }
