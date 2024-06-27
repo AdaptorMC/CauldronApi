822b37d69215da11fc2e4c3ba4c549e8fffa11ce
chai <chainot@hotmail.co.th>
2024-06-27 14:06:30 +0700

    Clean up Doc

--- src/main/java/net/yunitrish/adaptor/api/CauldronRecipeProvider.java
+++ src/main/java/net/yunitrish/adaptor/api/CauldronRecipeProvider.java
@@ -1,36 +1,25 @@
 package net.yunitrish.adaptor.api;
 
-/**
- * Interface for providing cauldron recipes.
- * <p>
- * Other mods can implement this interface to add their own cauldron recipes.
- * </p>
- *
- * <pre>
- * {@code
- * // Example usage in your mod's initialization class
- * public class YourModInit implements CauldronRecipeProvider {
- *     @Override
- *     public void addCauldronRecipes() {
- *         CauldronRecipe gravelRecipe = CauldronRecipe.NORMAL_RECIPE
- *             .setName("gravelRecipe")
- *             .setRecipeItem(Items.GRAVEL.getDefaultStack())
- *             .setResultItem(Items.SAND.getDefaultStack(), Items.FLINT.getDefaultStack());
- *         CauldronCookEvent.registerRecipe(gravelRecipe);
- *     }
- *
- *     public static void init() {
- *         CauldronCookEvent.registerRecipeProvider(new YourModInit());
- *     }
- * }
- * }
- * </pre>
- *
- * @see net.yunitrish.adaptor.init.ModCauldronRecipeInit
- */
 public interface CauldronRecipeProvider {
     /**
-     * Now Other mod Can call this method to implement into their own mod or other mod project
+     * Adds cauldron recipes to the registry.
+     * <p>
+     * Implement this method in your class to register custom cauldron recipes.
+     * </p>
+     *
+     * <pre>
+     * {@code
+     * public class MyModCauldronRecipes implements CauldronRecipeProvider {
+     *     @Override
+     *     public void addCauldronRecipes() {
+     *         CauldronRecipe recipe = new CauldronRecipe(CauldronRecipeRegistry.DeviceType.NORMAL, "MyRecipe")
+     *             .setRecipeItem(new ItemStack(Items.DIRT))
+     *             .setResultItem(new ItemStack(Items.DIAMOND));
+     *         CauldronRecipeRegistry.registerRecipe(recipe);
+     *     }
+     * }
+     * }
+     * </pre>
      */
     void addCauldronRecipes();
 }
