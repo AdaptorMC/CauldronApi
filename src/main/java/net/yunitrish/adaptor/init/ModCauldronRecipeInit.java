fa424676550333c5697225049c57040d3881dd35
chai <chainot@hotmail.co.th>
2024-06-27 15:35:40 +0700

    Whaty? space??

--- src/main/java/net/yunitrish/adaptor/init/ModCauldronRecipeInit.java
+++ src/main/java/net/yunitrish/adaptor/init/ModCauldronRecipeInit.java
@@ -5,7 +5,6 @@ import net.yunitrish.adaptor.api.CauldronRecipeProvider;
 import net.yunitrish.adaptor.api.CauldronRecipeRegistry;
 import net.yunitrish.adaptor.recipe.CauldronRecipe;
 
-
 public class ModCauldronRecipeInit implements CauldronRecipeProvider {
     @Override
     public void addCauldronRecipes() {
